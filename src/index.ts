0b1b88ed470e74253ef8500d8e1aa9c91547c63b
hariyebk <harunbekri6@gmail.com>
2023-10-05 15:46:40 +0300

    abstracted compare and swap for NumbersCollection

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,7 @@
-class Sorter {
-    constructor(public collection : number[]){}
-    sort() : void {
-        const {length} = this.collection
-        // buble sort
-        for(let i = 0; i < length; i++){
-            for(let j = 0; j < length - i - 1; j++){
-                if(this.collection[j] > this.collection[ j + 1]){
-                    let left = this.collection[j]
-                    // swap
-                    this.collection[j] = this.collection[j + 1]
-                    this.collection[j + 1] = left
-                }
-            }
+import Sorter from "./Sort"
+import NumbersCollection from "./NumbersCollection"
 
-        }
-
-    }
-}
-
-const sorter = new Sorter([10, 4, 6, -8])
+const numbersCollection = new NumbersCollection([10, 5, -4, 0 , 45])
+const sorter = new Sorter(numbersCollection)
 sorter.sort()
-console.log(sorter.collection)
+console.log(numbersCollection.data)
